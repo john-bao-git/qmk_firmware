331b22a9d10b2ced6dcf636c18717776d08f1623
John Bao <johncbao1@gmail.com>
2023-01-07 19:46:54 -0500

    Changed mouse buttons to stop the LT(1, KC_BTN2)

--- keyboards/ploopyco/trackball/keymaps/john-bao/keymap.c
+++ keyboards/ploopyco/trackball/keymaps/john-bao/keymap.c
@@ -22,11 +22,11 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT( 
         KC_BTN1, _______, KC_BTN3, 
-        LT(1, KC_BTN2),  MO(2)
+        KC_BTN2,  MO(1)
     ),
     [1] = LAYOUT( 
         KC_BTN4, _______, KC_BTN5, 
-        _______, _______ 
+        MO(2), _______
     ),    
     [2] = LAYOUT( 
         DRAG_SCROLL, _______, DPI_CONFIG, 
@@ -37,4 +37,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [5] = LAYOUT( _______, _______, _______, _______, _______ ),
     [6] = LAYOUT( _______, _______, _______, _______, _______ ),
     [7] = LAYOUT( _______, _______, _______, _______, _______ )
+    // [0] = LAYOUT( /* Base */
+    //     KC_BTN1, KC_BTN3, KC_BTN2,
+    //     KC_BTN4, KC_BTN5
+    // ),
 };
